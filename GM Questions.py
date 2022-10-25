c454143675aae198cc231aa54c8e99b68d0fc4c5
Sinead Keane <111061785+skeane0331@users.noreply.github.com>
2022-10-25 10:10:47 -0400

    Add files via upload

---
+++ GM Questions.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[19]:
+
+
+a = int(input())
+b = int(input())
+
+if (a > b):
+    print (a, ">", b)
+elif (a < b): 
+    print ( a, "<", b)
+else:
+    print (a, "=", b)
+
+
+# In[20]:
+
+
+import math
+x = int(input())
+if int(math.sqrt(x) + 0.5) ** 2 == x:
+    print("True")
+else:
+    print("False")
+
+
+# In[ ]:
+
+
+
+
